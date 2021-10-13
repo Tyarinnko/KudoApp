d2cb734fe4e2fbe6493e7a16f4961026a4a93a0e
taira <tyarinnko430@gmail.com>
2021-10-14 00:33:51 +0900

    チーム機能修正　詳細✕

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('logout/',Logout.as_view(),name='logout'),
     path('team_list/',TeamList.as_view(),name='team_list'),
     path('team_new/',TeamNew.as_view(),name='team_new'),
-    path('team_list/team_detail/<pk>/',TeamDetail.as_view(),name='team_detail'),    
-    path('team_list/team_detail/<pk>/team_edit/',TeamEdit.as_view(),name='team_edit'),
+    path('team_list/team_detail/<int:pk>/',TeamDetail.as_view(),name='team_detail'),    
+    path('team_list/team_detail/<int:pk>/team_edit/',TeamEdit.as_view(),name='team_edit'),
     ]
