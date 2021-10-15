dc517fa76274aa951ef22cf32b74e3c312bb6f05
taira <tyarinnko430@gmail.com>
2021-10-15 21:43:46 +0900

    チーム参加機能追加

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.db import models
-from django.db.models import fields
 from .models import Team
 
 class UserCreationForm(UserCreationForm):
